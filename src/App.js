882211fe22be6fa82b6f6e559627a5c71850049d
evey_alabi <evesho.alabi@tropixel.com.ng>
2020-04-08 13:26:04 +0100

    updating our app with a shop page, preview collection component and collection item component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
 
 
 function App() {
@@ -15,7 +11,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './preview-collection.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
+
+
+const PreviewCollection = ({ title, items }) => (
+   <div className='preview-Collection'>
+        <h1 className='title'>{ title.toUpperCase()}</h1>
+        <div className= 'preview'>
+            {items
+                .filter((item, idx) => idx < 4)
+                .map( ({ id, ...otherItemProps}) => (
+                    <CollectionItem key={id} {...otherItemProps} />
+                ))
+            }
+        </div>
+   </div>
+
+)
+
+export default PreviewCollection;

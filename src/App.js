db32438d6bf6a8be5fcb2d116c7fe70edbf73582
evey_alabi <evesho.alabi@tropixel.com.ng>
2020-04-09 20:07:51 +0100

    updated signIn page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUppage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={ SignInAndSignUppage } />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-sign-up.styles.scss';
+
+const SignInAndSignUppage = () => (
+    <div className='sign-in-sign-up'>
+    <SignIn />
+    </div>
+)
+
+export default SignInAndSignUppage;
